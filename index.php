057be611b836ed7d6f8d0d19a4c581ba9d1fb503
Mohammed Naji <m.naji1512@gmail.com>
2026-01-29 10:53:38 +0200

    new changes

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
-Mohammed Naji
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium amet quo nisi doloremque veritatis totam obcaecati maiores architecto iste consequatur nam, dolor vitae animi et sint tempora mollitia! Qui, sint?
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h1>Mohammed Naji</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio officia quasi sunt rerum eligendi! Aliquam, expedita nemo consequuntur ea fugit ratione itaque tempora sint voluptates aspernatur molestias deleniti animi cumque.</p>
+</body>
+
+</html>
